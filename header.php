e1f164be8b9b0865065fabfa7263c2b6616bbb8d
marquesdacosta <tomasmarquesdacosta@gmail.com>
2023-06-26 12:09:25 +0200

    Creation header+footer en php pour require_once

---
+++ header.php
@@ -0,0 +1,32 @@
+<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/style.css">
+
+</head>
+<body>
+    
+<header>
+        <nav>
+            <ul>
+                <li>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32.019 36.971">
+                        <path id="Tracé_16546" data-name="Tracé 16546" d="M16.009,23.109,0,13.862V27.73L8,32.35V23.109l4,2.31v9.241l4,2.31,4-2.31V25.419l4-2.31V32.35l8-4.621V13.862ZM32.019,0,16.009,9.241,0,0V9.241l16.009,9.247L32.019,9.241Z" fill="#938a56"/>
+                    </svg>
+                    
+                </li>
+                <li><a href="">pêche à la carpe</a></li>
+                <li><a href="">pêche au silure </a></li>
+                <li><a href="">matériel et appâts </a></li>
+                <li><a href="">blog</a></li>
+                <li><a href="">qui sommes-nous?</a></li>
+                <li><a href="">contact</a></li>
+            </ul>
+        </nav>
+</header>
+?>
